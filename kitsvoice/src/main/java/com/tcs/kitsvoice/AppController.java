3e4bff582a7f8bf5e8192c6fad70df8504e69dc8
mandas04 <mandas04@GBKTFDC013X0004.kits.kfplc.com>
2018-07-20 13:30:46 +0530

    Handling speech responses

--- kitsvoice/src/main/java/com/tcs/kitsvoice/AppController.java
+++ kitsvoice/src/main/java/com/tcs/kitsvoice/AppController.java
@@ -1,13 +1,21 @@
 package com.tcs.kitsvoice;
 
+import java.util.Arrays;
+
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.twilio.http.HttpMethod;
+import com.twilio.twiml.TwiMLException;
 import com.twilio.twiml.VoiceResponse;
+import com.twilio.twiml.messaging.Redirect;
+import com.twilio.twiml.voice.Gather;
+import com.twilio.twiml.voice.Pause;
 import com.twilio.twiml.voice.Say;
 
 @Controller
@@ -19,12 +27,39 @@ public class AppController {
 		return "hello world";
 	}
 
-	@RequestMapping(value = "/attendCall", method = RequestMethod.POST)
+	@RequestMapping(value = "/attendCall", method = RequestMethod.POST, produces = { "application/xml" })
 	@ResponseBody
 	public String attendCall() {
-		Say say = new Say.Builder("ultimatix.net server IP address could not be found.").build();
-		VoiceResponse voiceResponse = new VoiceResponse.Builder().say(say).build();
-		return voiceResponse.toXml();
+
+		Say say = new Say.Builder("Hello, How can I help you today?").build();
+		Gather gather = new Gather.Builder().inputs(Arrays.asList(Gather.Input.SPEECH)).action("/completed").partialResultCallback("/partial").method(HttpMethod.POST).speechTimeout("auto").numDigits(1).say(say).build();
+		Say say2 = new Say.Builder("We didn't receive any input. Goodbye!").build();
+
+		VoiceResponse response = new VoiceResponse.Builder().gather(gather).say(say2).build();
+
+		try {
+			System.out.println(response.toXml());
+		} catch (TwiMLException e) {
+			e.printStackTrace();
+		}
+		return response.toXml();
+
+		// Contact number: 448081689319
 	}
 
+	@RequestMapping(value = "/completed", method = RequestMethod.POST, produces = { "application/xml" })
+	@ResponseBody
+	public String finalresult(@RequestBody String r, @RequestParam("SpeechResult") String speechResult, @RequestParam("Confidence") double confidence) {
+		System.out.println(speechResult);
+		System.out.println(confidence);
+		return "<Response><Say>You have said " + speechResult + ". Thank you</Say></Response>";
+	}
+	
+
+	@RequestMapping(value = "/partial", method = RequestMethod.POST, produces = { "application/xml" })
+	@ResponseBody
+	public void partialresult(@RequestBody String r, @RequestParam("UnstableSpeechResult") String speechResult, @RequestParam("Stability") double stability) {
+		System.out.println(speechResult);
+		System.out.println(stability);
+	}
 }
